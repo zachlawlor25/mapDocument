dda75346caac516c8ce6cbfa644b7e6b8bc88dd5
Zach <zachlawlor25@gmail.com>
2019-03-21 16:30:11 -0400

    arcpy.mapping.MapDocument
    arcpy.mapping.MapDocument

---
+++ mapDocument.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 16:23:20 2019
+
+@author: tuj47049
+"""
+from __future__ import absolute_import, division, print_function 
+
+import arcpy, sys, os
+
+# System Arguments 'C:/Users/tuj47049/Desktop/Module/module.mxd'
+
+myMap = sys.argv[1]
+mxd = arcpy.mapping.MapDocument(myMap)
+print(mxd)
